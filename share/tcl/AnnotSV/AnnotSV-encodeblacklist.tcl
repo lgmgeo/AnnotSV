f501436392d9a69eb63f899cd1583c73131d3b57
lgmgeo <veronique.geoffroy@inserm.fr>
2021-01-15 15:47:04 +0100

    v3.0.3

--- share/tcl/AnnotSV/AnnotSV-encodeblacklist.tcl
+++ share/tcl/AnnotSV/AnnotSV-encodeblacklist.tcl
@@ -1,5 +1,5 @@
 ############################################################################################################
-# AnnotSV 3.0.2                                                                                            #
+# AnnotSV 3.0.3                                                                                            #
 #                                                                                                          #
 # AnnotSV: An integrated tool for Structural Variations annotation and ranking                             #
 #                                                                                                          #
