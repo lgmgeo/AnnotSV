08670411e1b1c2cd422d27def32801aa36836190
Pierre Marijon <pierre.marijon-ext@aphp.fr>
2022-12-13 16:08:30 +0100

    fix: searchForAFreePortNumber

--- share/bash/AnnotSV/searchForAFreePortNumber.bash
+++ share/bash/AnnotSV/searchForAFreePortNumber.bash
@@ -1,6 +1,19 @@
 #!/bin/bash
-ss -tln |
-  awk 'NR > 1{gsub(/.*:/,"",$4); print $4}' |
-  sort -un |
-  awk -v n=50000 '$0 < n {next}; $0 == n {n++; next}; {exit}; END {print n}'
 
+# com: Compare two sorted files line by line. Output the lines that are common, plus the lines that are unique.
+#      comm [options]... File1 File2
+#       -2 Suppress lines unique to file2
+#       -3 Suppress lines that appear in both files
+#
+# seq: print a sorted sequence of numbers from 5000 to 50000
+#
+# ss: used to dump socket statistics.
+#       -H, --no-header
+#       -n, --numeric
+#              Do not try to resolve service names. Show exact bandwidth
+#              values, instead of human-readable.
+#       -t, --tcp
+#              Display TCP sockets.
+#     e.g. "ss -Htan" >>> "LISTEN    0      5                       0.0.0.0:63864                  0.0.0.0:*"
+
+comm -23 <(seq 5000 50000 | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | head -n 1
