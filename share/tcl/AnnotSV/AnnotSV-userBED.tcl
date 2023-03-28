e14e8606a8eada49d25a0fdaf02e3aad76529c2d
lgmgeo <veronique.geoffroy@inserm.fr>
2023-03-28 17:12:16 +0200

    v3.3.2

--- share/tcl/AnnotSV/AnnotSV-userBED.tcl
+++ share/tcl/AnnotSV/AnnotSV-userBED.tcl
@@ -1,5 +1,5 @@
 ############################################################################################################
-# AnnotSV 3.3.1                                                                                            #
+# AnnotSV 3.3.2                                                                                            #
 #                                                                                                          #
 # AnnotSV: An integrated tool for Structural Variations annotation and ranking                             #
 #                                                                                                          #
