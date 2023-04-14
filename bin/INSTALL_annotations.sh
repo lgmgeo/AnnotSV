a84936564b0e939bd46b14fab272faeaca8321df
lgmgeo <veronique.geoffroy@inserm.fr>
2023-04-14 13:52:28 +0200

    v3.3.5

--- bin/INSTALL_annotations.sh
+++ bin/INSTALL_annotations.sh
@@ -12,9 +12,9 @@
 # Installation of human annotation
 mkdir ./AnnotSV_annotations
 cd ./AnnotSV_annotations
-curl -C - -LO https://www.lbgi.fr/~geoffroy/Annotations/Annotations_Human_3.3.4.tar.gz
-tar -xf Annotations_Human_3.3.4.tar.gz -C .
-rm -rf Annotations_Human_3.3.4.tar.gz
+curl -C - -LO https://www.lbgi.fr/~geoffroy/Annotations/Annotations_Human_3.3.5.tar.gz
+tar -xf Annotations_Human_3.3.5.tar.gz -C .
+rm -rf Annotations_Human_3.3.5.tar.gz
 
 
 # Installation of Exomiser data:
--- share/tcl/AnnotSV/AnnotSV-write.tcl
+++ share/tcl/AnnotSV/AnnotSV-write.tcl
@@ -1,5 +1,5 @@
 ############################################################################################################
-# AnnotSV 3.3.4                                                                                            #
+# AnnotSV 3.3.5                                                                                            #
 #                                                                                                          #
 # AnnotSV: An integrated tool for Structural Variations annotation and ranking                             #
 #                                                                                                          #
